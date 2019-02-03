27a52c60eadc9d321f37210799dc7ed2e176760b
arthurOcg <arthur.lima.oliveira@ccc.ufcg.edu.br>
2019-02-02 21:37:53 -0300

    Criacao de componente para listar os jogadores

---
+++ src/app/lista-jogador/lista-jogador.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ListaJogadorComponent } from './lista-jogador.component';
+
+describe('ListaJogadorComponent', () => {
+  let component: ListaJogadorComponent;
+  let fixture: ComponentFixture<ListaJogadorComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ListaJogadorComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ListaJogadorComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
